9f9ef35cf86c6c4058b3f97a8c425691989412fe
Abhinav Mishra <abhimishra142@gmail.com>
2026-04-16 09:19:39 +0530

    Added seed script using bulk insert for 10k employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+first_names = File.readlines(Rails.root.join('db/first_names.txt')).map(&:strip)
+last_names = File.readlines(Rails.root.join('db/last_names.txt')).map(&:strip)
+
+Employee.delete_all #Deleting existing record as engineers will run this script regularly
+
+employees = []
+
+10000.times do
+  first = first_names.sample
+  last = last_names.sample
+
+  employees << {
+    first_name: first,
+    last_name: last,
+    job_title: ["Engineer", "Manager", "HR"].sample,
+    country: ["India", "USA", "UK"].sample,
+    salary: rand(30000..150000),
+    created_at: Time.now,
+    updated_at: Time.now
+  }
+end
+
+Employee.insert_all(employees)
