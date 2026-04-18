0a2894d59f3ce413a9ea63547063fe319ad2a46c
Abhinav Mishra <abhimishra142@gmail.com>
2026-04-18 15:06:58 +0530

    enhance model validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,8 @@
 class Employee < ApplicationRecord
   validates :first_name, :last_name, :job_title, :country, :salary, presence: true
+  validates :salary, numericality: { greater_than: 0 }
+  validates :first_name, :last_name, :job_title, :country, length: { maximum: 100 }
+  validates_uniqueness_of :first_name, scope: [:last_name, :job_title, :country], message: "An employee with the same name, job title, and country already exists"
 
   def full_name
     "#{first_name} #{last_name}"
